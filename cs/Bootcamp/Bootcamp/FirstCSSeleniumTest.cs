f5d77d814649a14969992e139cf0039ea81c729c
Welmoed Bruring <welmoed.bruring@polteq.com>
2017-12-27 21:27:32 +0100

    CS project

---
+++ cs/Bootcamp/Bootcamp/FirstCSSeleniumTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using NFluent;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Bootcamp
+{
+    [TestClass]
+    public class FirstCSSeleniumTest
+    {
+        [TestMethod]
+        public void LogInSuccesful()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            // Open website
+            driver.Navigate().GoToUrl("https://techblog.polteq.com/testshop/index.php");
+
+            // Find and click the login button
+            driver.FindElement(By.ClassName("login")).Click();
+
+            // Enter email address
+            driver.FindElement(By.Id("email")).SendKeys("welmoed.bruring@polteq.com");
+
+            // Enter password
+            driver.FindElement(By.Id("passwd")).SendKeys("testw8woord");
+
+            // Click login button
+            driver.FindElement(By.Id("SubmitLogin")).Click();
+            
+            // Check that the My Account page is displayed
+            String actualPageHeading = driver.FindElement(By.ClassName("page-heading")).Text;
+            String expectedPageHeading = "MY ACCOUNT";
+            //Check.That(actualPageHeading).Equals(expectedPageHeading);
+
+            Assert.AreEqual(actualPageHeading, expectedPageHeading);
+
+            // Termiate the browser
+            driver.Quit();
+
+        }
+    }
+}
---
+++ cs/Bootcamp/Bootcamp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bootcamp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bootcamp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89eb585d-e346-478a-9599-ca106d30a934")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

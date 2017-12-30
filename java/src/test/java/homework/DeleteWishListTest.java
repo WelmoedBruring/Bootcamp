9d13f178b9855acbfbd1b3869ed64d4b15776b7b
Welmoed Bruring <welmoed.bruring@polteq.com>
2017-12-30 18:52:22 +0100

    Homework 0.9

--- java/src/test/java/homework/DeleteWishListTest.java
+++ java/src/test/java/homework/DeleteWishListTest.java
@@ -1,20 +1,16 @@
 package homework;
 
 import chapterSix.TestShopScenario;
+import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class DeleteWishListTest extends TestShopScenario {
-
-    private List<WebElement> columns;
-    private List<String> columnNames;
-    private List<WebElement> rows;
-    private List<String> rowNames;
-    private String listToDelete = "Feel the pain";
 
     @Test
     public void deleteWishListTest() throws Exception {
@@ -22,78 +18,75 @@ public class DeleteWishListTest extends TestShopScenario {
         login("welmoed@bruring-looge.com","1qazxsw2");
         driver.findElement(By.cssSelector(".icon-heart")).click();
 
+        String listToDelete = "Feel the pain";
+        int indexBeforeDelete = findWishList(listToDelete);
+        System.out.println("Index before deletion = " + indexBeforeDelete + "\nDeleting wish list...");
         deleteWishList(listToDelete);
-        //TODO Write assertion to check that wish list is successfully deleted & no longer exists
-//        Assertions.assertThat(rowNames.)
-        //TODO Write addWishList
-        addWishList(listToDelete);
-        //TODO Write assertion to check that list is successfully added --> findWishList class that can be used by both deleteWishList and the assertions?
 
+        int indexAfterDelete = findWishList(listToDelete);
+        System.out.println("Wish list is deleted. \nIndex after deletion = " + indexAfterDelete);
+        System.out.println(getRows());
+
+
+        Assertions.assertThat(indexAfterDelete).
+                as("The returned index should be 0, indicating that the wish list does not exist").isEqualTo(0);
+
+        addWishList(listToDelete);
+
+        int indexAfterAdd = findWishList(listToDelete);
+        System.out.println("Index after adding = " + indexAfterAdd);
+        System.out.println(getRows());
+
+        Assertions.assertThat(indexAfterAdd).
+                as("The returned index should be greater 0, indicating that the wish list exists").isGreaterThan(0);
     }
 
     private void addWishList(String listToDelete) {
-
+        WebElement textbox = driver.findElement(By.cssSelector("#name"));
+        textbox.sendKeys(listToDelete);
+        driver.findElement(By.cssSelector("#submitWishlist")).click();
+        driver.navigate().refresh();
+        wait.until(ExpectedConditions.refreshed(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".table-bordered"))));
     }
 
-    private void deleteWishList(String listToDelete) throws Exception {
-        boolean found = false;
-
-        getColumns();
-        getRows();
-        // For each row i in the table body (tbody)
-        for(int i = 0; i<rows.size(); i++) {
-            WebElement row = driver.findElement(By.xpath("(//tbody/tr)["+(i+1)+"]"));
-            System.out.println(row.getText());
-            if(row.getText().contains(listToDelete)){
-                System.out.println("This is the row you are looking for!");
-                // For each cell in row i
-
-                WebElement deleteButton = driver.findElement(By.xpath(
-                        "(//tbody/tr)[" + (i + 1) + "]/td[@class='wishlist_delete']/a"));
-                deleteButton.click();
-                driver.switchTo().alert().accept();
-                found = true;
-                break;
-            }
-            if(found){
-                break;
-            } else {
-                throw new Exception("The wish list you were looking for does not exist!");
-            }
+    private void deleteWishList(String listToDelete) {
+        int index = findWishList(listToDelete);
+        if(index > 0) {
+//            System.out.println("Index: "+ index);
+            WebElement deleteButton = driver.findElement(By.xpath(
+                    "(//tbody/tr)[" + (index) + "]/td[@class='wishlist_delete']/a"));
+            deleteButton.click();
+            wait.until(ExpectedConditions.alertIsPresent());
+            driver.switchTo().alert().accept();
+            wait.until(ExpectedConditions.not(ExpectedConditions.alertIsPresent()));
+            driver.navigate().refresh();
+        } else {
+            throw new NoSuchElementException("The wishlist you want to delete does not exist!");
         }
     }
 
-    // Counts the number of rows in the table and saves their text to a list
+    private int findWishList(String wishList) {
+        int index = 0;  // Index is initialized at 0; if returned with value 0 it means that the wish list was not found
+        // For each row i in the table body, check if it contains the name of the target wish list
+        int numberOfRows = getRows();
+
+        for(int i = 0; i<numberOfRows; i++) {
+            WebElement row = driver.findElement(By.xpath("(//tbody/tr)["+(i+1)+"]"));
+            if(row.getText().contains(wishList)) {
+                index = i + 1; // +1 Because xpath is not 0-based
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    // Returns the number of rows in the table
     private int getRows() {
         // Get number of rows
-        rows = driver.findElements(By.xpath("//tbody/tr"));
-        getRowNames();
-
-        System.out.println("There are "+ rows.size() + " rows: " + rowNames);
+        List<WebElement> rows = driver.findElements(By.xpath("//tbody/tr"));
+        System.out.println("There are "+ rows.size() + " rows");
         return rows.size();
-    }
-
-    private void getRowNames() {
-
-        rowNames = new ArrayList<>();
-
-        for(int i = 0; i< rows.size(); i++) {
-            String text = rows.get(i).getText();
-            rowNames.add(text);
-        }
-    }
-
-    // Counts the number of columns in the table and saves their names to a list
-    private int getColumns() {
-        // Get number of columns & column headers
-        columns = driver.findElements(By.xpath("//th"));
-        columnNames = new ArrayList<>();
-        for(int i = 0; i < columns.size(); i++) {
-            String text = columns.get(i).getText();
-            columnNames.add(text);
-        }
-        System.out.println("There are "+ columns.size() + " columns: " + columnNames);
-        return columns.size();
     }
 
     private void login(String email, String password) {
